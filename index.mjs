deb5d1a2abde7dabf7ebc2635aa2c8557aa55850
Asia Grady <66960776+asiakay@users.noreply.github.com>
2021-05-06 08:20:14 -0400

    merged remote

--- index.mjs
+++ index.mjs
@@ -1,114 +1,88 @@
-<<<<<<< HEAD
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+'reach 0.1';
 
-(async () => {
-  const stdlib = await loadStdlib();
-  const startingBalance = stdlib.parseCurrency(10);
-  const accAlice = await stdlib.newTestAccount(startingBalance);
-  const accBob = await stdlib.newTestAccount(startingBalance);
+const [ isHand, ROCK, PAPER, SCISSORS ] = makeEnum(3);
+const [ isOutcome, B_WINS, DRAW, A_WINS ] = makeEnum(3);
 
-  const fmt = (x) => stdlib.formatCurrency(x, 4);
-  const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
-  const beforeAlice = await getBalance(accAlice);
-  const beforeBob = await getBalance(accBob);
+const winner = (handA, handB) =>
+      ((handA + (4 - handB)) % 3);
 
-  const ctcAlice = accAlice.deploy(backend);
-  const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
+assert(winner(ROCK, PAPER) == B_WINS);
+assert(winner(PAPER, ROCK) == A_WINS);
+assert(winner(ROCK, ROCK) == DRAW);
 
-  const HAND = ['Rock', 'Paper', 'Scissors'];
-  const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
-  const Player = (Who) => ({
-    ...stdlib.hasRandom, 
-    getHand: async () => {
-      const hand = Math.floor(Math.random() * 3);
-      console.log(`${Who} played ${HAND[hand]}`);
-      if ( Math.random() <= 0.01 ) { // moves accept wager timeout code into method, makes so timeouts happen 1% of the time
-        for ( let i = 0; i < 10; i++ ) {
-          console.log(`  ${Who} takes their sweet time sending it back...`);
-          await stdlib.wait(1);
-        }
-      }
-      return hand;
-    },
-    seeOutcome: (outcome) => {
-      console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
-    },
-    informTimeout: () => {
-      console.log(`${Who} observed a timeout`);
-    },
-  });
+forall(UInt, handA =>
+  forall(UInt, handB =>
+    assert(isOutcome(winner(handA, handB)))));
 
-  await Promise.all([
-    backend.Alice(ctcAlice, {
-      ...Player('Alice'),
-      wager: stdlib.parseCurrency(5),
-    }),
-    backend.Bob(ctcBob, {
-      ...Player('Bob'),
-      acceptWager: (amt) => { // redefining Bob's accetpWager method as an async function 
-          console.log(`Bob accepts the wager of ${fmt(amt)}.`);
-        },
-    }),
-  ]);
+forall(UInt, (hand) =>
+  assert(winner(hand, hand) == DRAW));
 
-  const afterAlice = await getBalance(accAlice);
-  const afterBob = await getBalance(accBob);
+const Player =
+      { ...hasRandom, // <--- new!
+        getHand: Fun([], UInt),
+        seeOutcome: Fun([UInt], Null),
+        informTimeout: Fun([], Null) }; // informTimeout, that receives no arguments and returns no information
+const Alice =
+      { ...Player,
+        wager: UInt };
+const Bob =
+      { ...Player,
+        acceptWager: Fun([UInt], Null) };
 
-  console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
-  console.log(`Bob went from ${beforeBob} to ${afterBob}.`);
+const DEADLINE = 10; // defines the deadline as ten time delta units, https://docs.reach.sh/ref-model.html#%28tech._time._delta%29
+export const main =
+  Reach.App(
+    {},
+    [Participant('Alice', Alice), Participant('Bob', Bob)],
+    (A, B) => {
+      const informTimeout = () => { // defines the function as an arrow expression.
+        each([A, B], () => { // has each of the participants perform a local step.
+          interact.informTimeout(); }); }; // has them call the new informTimeout method.
 
-})();
-=======
-import { loadStdlib } from '@reach-sh/stdlib'; // imports Reach standard library loader
-import * as backend from './build/index.main.mjs'; // imports backend to be compiled by ./reach
+      A.only(() => {
+        const wager = declassify(interact.wager); });
+      A.publish(wager) // Alice publishing and payingn wager
+        .pay(wager);
+      commit();
+      B.only(() => {
+        interact.acceptWager(wager); });
+      B.pay(wager) // bob pays wager 
+        .timeout(DEADLINE, () => closeTo(A, informTimeout)); // adds a timeout handler to Bob’s publication
+      // no consensus step commit
+      var outcome = DRAW; // defines the loop variable, outcome
+      invariant(balance() == 2 * wager && isOutcome(outcome) ); // the body of the loop does not change the balance in the contract account and that outcome is a valid outcome
+      while ( outcome == DRAW ) { // begins the loop with the condition that it continues as long as the outcome is a draw
+        
+        commit(); // commits the last transaction, which at the start of the loop is Bob’s acceptance of the wager, and at subsequent runs of the loop is Alice’s publication of her hand.
+ 
+      A.only(() => {
+          const _handA = interact.getHand();
+          const [_commitA, _saltA] = makeCommitment(interact, _handA);
+          const commitA = declassify(_commitA); });
+      A.publish(commitA)
+          .timeout(DEADLINE, () => closeTo(B, informTimeout));
+        commit(); // almost identical to the older version, except the wager is already known and paid.
+      
+      unknowable(B, A(_handA, _saltA));
+      B.only(() => {
+        const handB = declassify(interact.getHand()); });
+      B.publish(handB)
+      .timeout(DEADLINE, () => closeTo(A, informTimeout));
+      commit(); // Bob has already accepted and paid the wager prior to this step
 
-(async () => { // asynchronous function & body of our frontend
-  const stdlib = await loadStdlib(); // loads the standard library dynamically based on the REACH_CONNECTOR_MODE
-  const startingBalance = stdlib.parseCurrency(10); // defines a quantity of network tokens as the starting balance for each test account.
-  const accAlice = await stdlib.newTestAccount(startingBalance); // test accounts with initial endowments for Alice and Bob.
-  const accBob = await stdlib.newTestAccount(startingBalance);
+      A.only(() => {
+        const [saltA, handA] = declassify([_saltA, _handA]); });
+      A.publish(saltA, handA)
+        .timeout(DEADLINE, () => closeTo(B, informTimeout)); // adds a timeout handler to Alice's publication
+      checkCommitment(commitA, saltA, handA); // almost an identical step as Bob's previous
 
-  const fmt = (x) => stdlib.formatCurrency(x, 4); // function for displaying currency amounts with up to 4 decimal places
-  const getBalance = async (who) => fmt(await stdlib.balaceOf(who)); // function for getting the balance of a participant and displaying it with up to 4 decimal places
-  const beforeAlice = await getBalance(accAlice); // get the balance before the game starts for both Alice 
-  const beforeBob = await getBalance(accBob); // get the balance before the game starts for Bob
+      outcome = winner(handA, handB); // updates loop with new value
+      continue; } // continues the loop , while doesn't run infinitely
 
-  const ctcAlice = accAlice.deploy(backend); //  Alice deploys the application
-  const ctcBob = accBob.attach(backend, ctcAlice.getInfo()); // Bob attaches to it
-
-  const HAND = ['Rock', 'Paper', 'Scissors']; // array to hold the meaning of the hands
-  const OUTCOME = ['Bob wins', 'Draw', 'Alice wins']; // arrays to hold the outcomes
-  const Player = (Who) => ({ // constructor for the Player implementation
-    getHand: () => { // defines the getHand method
-      const hand = Math.floor(Math.random() * 3);
-      console.log(`${Who} played ${HAND[hand]}`);
-      return hand;
-    },
-    seeOutcome: (outcome) => { // defines the seeOutcome method
-      console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
-    },
-  });
-
-  await Promise.all([ // waits for the backends to complete
-    backend.Alice( ctcAlice, {
-      ...Player('Alice'),
-      wager: stdlib.parseCurrency(5),
-      }), // initializes & Instantiates Alice’s backend
-    backend.Bob(ctcBob, {
-      ...Player('Bob'),
-      acceptWager: (amt) => { // defining the acceptWager function
-        console.log(`Bob accepts the wager of ${fmt(amt)}.`);
-      },
-    }), // initializes & Instantiates Bob’s backend
-  ]);
-
-  const afterAlice = await getBalance(accAlice); // gets balance after
-  const afterBob = await getBalance(accBob); // gets balance after
-
-  console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`); // prints to console
-  console.log(`Bob went from ${beforeBob} to ${afterBob}.`); // prints to console
-
-
-})(); // <-- Don't forget these! They call this asynchronous function that we’ve defined
->>>>>>> 8775599839d02d82028646157bd205b0c1c9d23c
+     assert(outcome == A_WINS || outcome == B_WINS); // asserts that outcome is never draw
+     transfer(2 * wager).to(outcome == A_WINS ? A : B); // transfers funds to winner
+     commit();
+     
+      each([A, B], () => {
+        interact.seeOutcome(outcome); });
+      exit(); });
--- index.rsh
+++ index.rsh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'reach 0.1';
 
 const [ isHand, ROCK, PAPER, SCISSORS ] = makeEnum(3);
@@ -19,10 +18,10 @@ forall(UInt, (hand) =>
   assert(winner(hand, hand) == DRAW));
 
 const Player =
-      { ...hasRandom, // <--- new!
+      { ...hasRandom,
         getHand: Fun([], UInt),
         seeOutcome: Fun([UInt], Null),
-        informTimeout: Fun([], Null) }; // informTimeout, that receives no arguments and returns no information
+        informTimeout: Fun([], Null) };
 const Alice =
       { ...Player,
         wager: UInt };
@@ -30,102 +29,60 @@ const Bob =
       { ...Player,
         acceptWager: Fun([UInt], Null) };
 
-const DEADLINE = 10; // defines the deadline as ten time delta units, https://docs.reach.sh/ref-model.html#%28tech._time._delta%29
+const DEADLINE = 30;
 export const main =
   Reach.App(
     {},
     [Participant('Alice', Alice), Participant('Bob', Bob)],
     (A, B) => {
-      const informTimeout = () => { // defines the function as an arrow expression.
-        each([A, B], () => { // has each of the participants perform a local step.
-          interact.informTimeout(); }); }; // has them call the new informTimeout method.
+      const informTimeout = () => {
+        each([A, B], () => {
+          interact.informTimeout(); }); };
 
       A.only(() => {
         const wager = declassify(interact.wager); });
-      A.publish(wager) // Alice publishing and payingn wager
+      A.publish(wager)
         .pay(wager);
       commit();
+
       B.only(() => {
         interact.acceptWager(wager); });
-      B.pay(wager) // bob pays wager 
-        .timeout(DEADLINE, () => closeTo(A, informTimeout)); // adds a timeout handler to Bob’s publication
-      // no consensus step commit
-      var outcome = DRAW; // defines the loop variable, outcome
-      invariant(balance() == 2 * wager && isOutcome(outcome) ); // the body of the loop does not change the balance in the contract account and that outcome is a valid outcome
-      while ( outcome == DRAW ) { // begins the loop with the condition that it continues as long as the outcome is a draw
-        
-        commit(); // commits the last transaction, which at the start of the loop is Bob’s acceptance of the wager, and at subsequent runs of the loop is Alice’s publication of her hand.
- 
-      A.only(() => {
+      B.pay(wager)
+        .timeout(DEADLINE, () => closeTo(A, informTimeout));
+
+      var outcome = DRAW;
+      invariant(balance() == 2 * wager && isOutcome(outcome) );
+      while ( outcome == DRAW ) {
+        commit();
+
+        A.only(() => {
           const _handA = interact.getHand();
           const [_commitA, _saltA] = makeCommitment(interact, _handA);
           const commitA = declassify(_commitA); });
-      A.publish(commitA)
+        A.publish(commitA)
           .timeout(DEADLINE, () => closeTo(B, informTimeout));
-        commit(); // almost identical to the older version, except the wager is already known and paid.
-      
-      unknowable(B, A(_handA, _saltA));
-      B.only(() => {
-        const handB = declassify(interact.getHand()); });
-      B.publish(handB)
-      .timeout(DEADLINE, () => closeTo(A, informTimeout));
-      commit(); // Bob has already accepted and paid the wager prior to this step
+        commit();
 
-      A.only(() => {
-        const [saltA, handA] = declassify([_saltA, _handA]); });
-      A.publish(saltA, handA)
-        .timeout(DEADLINE, () => closeTo(B, informTimeout)); // adds a timeout handler to Alice's publication
-      checkCommitment(commitA, saltA, handA); // almost an identical step as Bob's previous
+        unknowable(B, A(_handA, _saltA));
+        B.only(() => {
+          const handB = declassify(interact.getHand()); });
+        B.publish(handB)
+          .timeout(DEADLINE, () => closeTo(A, informTimeout));
+        commit();
 
-      outcome = winner(handA, handB); // updates loop with new value
-      continue; } // continues the loop , while doesn't run infinitely
+        A.only(() => {
+          const [saltA, handA] = declassify([_saltA, _handA]); });
+        A.publish(saltA, handA)
+          .timeout(DEADLINE, () => closeTo(B, informTimeout));
+        checkCommitment(commitA, saltA, handA);
 
-     assert(outcome == A_WINS || outcome == B_WINS); // asserts that outcome is never draw
-     transfer(2 * wager).to(outcome == A_WINS ? A : B); // transfers funds to winner
-     commit();
-     
+        outcome = winner(handA, handB);
+        continue; }
+
+      assert(outcome == A_WINS || outcome == B_WINS);
+      transfer(2 * wager).to(outcome == A_WINS ? A : B);
+      commit();
+
       each([A, B], () => {
         interact.seeOutcome(outcome); });
       exit(); });
-=======
-'reach 0.1'; 
-const Player =  // participant interact interface shared between two players
-    {
-        getHand: Fun([], UInt), // method 1 returns a number
-        seeOutcome: Fun([UInt], Null) }; // method 2 receives a number
-const Alice =  // defining Alice's interface as Player interface 
-      { ...Player,
-        wager: UInt }; // plus integer value called wager
-const Bob = // // defining Bob's interface as Player interface 
-      { ... Player,
-        acceptWager: Fun([UInt], Null) }; // plus integer value called acceptWager
-
-export const main =
-  Reach.App(
-    {},
-    [Participant('Alice', Player), Participant('Bob', Player)], // interface for both participants
-    (A, B) => {
-      A.only(() => { // Only alice is bound to handA
-        const handA = declassify(interact.getHand()); }); // declassifies and binds value to Alice through getHand method
-      A.publish(handA); // tells Alice to join the app by publishing to the consensus network
-      commit(); // commits the state to the consensus network
-      
-      B.only(() => { // matching Alice's local step
-        const handB = declassify(interact.getHand()); });
-      B.publish(handB); // through consensus transfer publication https://docs.reach.sh/ref-model.html#%28tech._consensus._transfer%29
-
-      const outcome = (handA + (4 - handB)) % 3; // computes the outcome of the game before committing
-      commit();
-    
-      each([A, B], () => { // https://docs.reach.sh/ref-programs-step.html#%28tech._each%29
-        interact.seeOutcome(outcome); }); // https://docs.reach.sh/ref-programs-local.html#%28reach._%28%28interact%29%29%29
-
-      exit(); });
-
-
-      /* interact in the rest of the program 
-      will be bound to an object with methods 
-      corresponding to these actions, 
-      which will connect to the frontend 
-      of the corresponding participant. */
->>>>>>> 8775599839d02d82028646157bd205b0c1c9d23c
