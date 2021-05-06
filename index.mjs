f326503c7b019ea85a02c2c1142da910efa10ee0
Asia Grady <66960776+asiakay@users.noreply.github.com>
2021-05-06 03:41:00 -0400

    changes to frontend dealing with presentation and interfacing

--- index.mjs
+++ index.mjs
@@ -4,9 +4,13 @@ import * as backend from './build/index.main.mjs'; // imports backend to be comp
 (async () => { // asynchronous function & body of our frontend
   const stdlib = await loadStdlib(); // loads the standard library dynamically based on the REACH_CONNECTOR_MODE
   const startingBalance = stdlib.parseCurrency(10); // defines a quantity of network tokens as the starting balance for each test account.
-
   const accAlice = await stdlib.newTestAccount(startingBalance); // test accounts with initial endowments for Alice and Bob.
   const accBob = await stdlib.newTestAccount(startingBalance);
+
+  const fmt = (x) => stdlib.formatCurrency(x, 4); // function for displaying currency amounts with up to 4 decimal places
+  const getBalance = async (who) => fmt(await stdlib.balaceOf(who)); // function for getting the balance of a participant and displaying it with up to 4 decimal places
+  const beforeAlice = await getBalance(accAlice); // get the balance before the game starts for both Alice 
+  const beforeBob = await getBalance(accBob); // get the balance before the game starts for Bob
 
   const ctcAlice = accAlice.deploy(backend); //  Alice deploys the application
   const ctcBob = accBob.attach(backend, ctcAlice.getInfo()); // Bob attaches to it
@@ -25,13 +29,23 @@ import * as backend from './build/index.main.mjs'; // imports backend to be comp
   });
 
   await Promise.all([ // waits for the backends to complete
-    backend.Alice(
-      ctcAlice,
-      Player('Alice'),
-    ), // initializes & Instantiates Alice’s backend
-    backend.Bob(
-      ctcBob,
-      Player('Bob'),
-    ), // initializes & Instantiates Bob’s backend
+    backend.Alice( ctcAlice, {
+      ...Player('Alice'),
+      wager: stdlib.parseCurrency(5),
+      }), // initializes & Instantiates Alice’s backend
+    backend.Bob(ctcBob, {
+      ...Player('Bob'),
+      acceptWager: (amt) => { // defining the acceptWager function
+        console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+      },
+    }), // initializes & Instantiates Bob’s backend
   ]);
+
+  const afterAlice = await getBalance(accAlice); // gets balance after
+  const afterBob = await getBalance(accBob); // gets balance after
+
+  console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`); // prints to console
+  console.log(`Bob went from ${beforeBob} to ${afterBob}.`); // prints to console
+
+
 })(); // <-- Don't forget these! They call this asynchronous function that we’ve defined
--- index.rsh
+++ index.rsh
@@ -1,5 +1,4 @@
 'reach 0.1'; 
-
 const Player =  // participant interact interface shared between two players
     {
         getHand: Fun([], UInt), // method 1 returns a number
@@ -8,8 +7,23 @@ const Player =  // participant interact interface shared between two players
 export const main =
   Reach.App(
     {},
-    [Participant('Alice', {}), Participant('Bob', {})], // interface for both participants
+    [Participant('Alice', Player), Participant('Bob', Player)], // interface for both participants
     (A, B) => {
+      A.only(() => { // Only alice is bound to handA
+        const handA = declassify(interact.getHand()); }); // declassifies and binds value to Alice through getHand method
+      A.publish(handA); // tells Alice to join the app by publishing to the consensus network
+      commit(); // commits the state to the consensus network
+      
+      B.only(() => { // matching Alice's local step
+        const handB = declassify(interact.getHand()); });
+      B.publish(handB); // through consensus transfer publication https://docs.reach.sh/ref-model.html#%28tech._consensus._transfer%29
+
+      const outcome = (handA + (4 - handB)) % 3; // computes the outcome of the game before committing
+      commit();
+    
+      each([A, B], () => { // https://docs.reach.sh/ref-programs-step.html#%28tech._each%29
+        interact.seeOutcome(outcome); }); // https://docs.reach.sh/ref-programs-local.html#%28reach._%28%28interact%29%29%29
+
       exit(); });
 
 
