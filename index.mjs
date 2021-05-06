f0b0e4bc96be65b5701600babb6c489e443ef6c7
Asia Grady <66960776+asiakay@users.noreply.github.com>
2021-05-06 02:24:20 -0400

    adds shell for JS frontend

--- index.mjs
+++ index.mjs
@@ -1,24 +1,24 @@
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+import { loadStdlib } from '@reach-sh/stdlib'; // imports Reach standard library loader
+import * as backend from './build/index.main.mjs'; // imports backend to be compiled by ./reach
 
-(async () => {
-  const stdlib = await loadStdlib();
-  const startingBalance = stdlib.parseCurrency(10);
+(async () => { // asynchronous function & body of our frontend
+  const stdlib = await loadStdlib(); // loads the standard library dynamically based on the REACH_CONNECTOR_MODE
+  const startingBalance = stdlib.parseCurrency(10); // defines a quantity of network tokens as the starting balance for each test account.
 
-  const accAlice = await stdlib.newTestAccount(startingBalance);
+  const accAlice = await stdlib.newTestAccount(startingBalance); // test accounts with initial endowments for Alice and Bob.
   const accBob = await stdlib.newTestAccount(startingBalance);
 
-  const ctcAlice = accAlice.deploy(backend);
-  const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
+  const ctcAlice = accAlice.deploy(backend); //  Alice deploys the application
+  const ctcBob = accBob.attach(backend, ctcAlice.getInfo()); // Bob attaches to it
 
-  await Promise.all([
+  await Promise.all([ // waits for the backends to complete
     backend.Alice(
       ctcAlice,
       {},
-    ),
+    ), // initializes Alice’s backend
     backend.Bob(
       ctcBob,
       {},
-    ),
+    ), // initializes Bob’s backend
   ]);
-})(); // <-- Don't forget these!
+})(); // <-- Don't forget these! They call this asynchronous function that we’ve defined
